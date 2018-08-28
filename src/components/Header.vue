939a9905a5e7fc0f15dd00c0e355a0ffe3251a98
Harish Krishnan <harishkrishnan1993@gmail.com>
2018-08-28 22:40:40 +0530

    Added selling and buying stocks

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,9 +10,10 @@
           <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
           <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
       </ul>
+      <strong class="navbar-text navbar-right">Funds: {{funds | currency}}</strong>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">End Day</a></li>
-        <li class="dropdown">
+        <li><a @click="endDay">End Day</a></li>
+        <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="#">Save Data</a></li>
@@ -24,3 +25,27 @@
   </div><!-- /.container-fluid -->
 </nav>
 </template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    };
+  },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions(['randomizeStocks']),
+    endDay() {
+      this.randomizeStocks();
+    }
+  }
+};
+</script>
+
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -8,15 +8,21 @@
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity" :class="{danger: insufficientFunds}">
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Buy</button>
+                    <button class="btn btn-success" @click="buyStock" :disabled="insufficientFunds ||quantity <= 0 || !Number.isInteger(quantity)">{{insufficientFunds ? 'Insufficient Funds': 'Buy'}}</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+.danger {
+  border: 1px solid red;
+}
+</style>
+
 
 <script>
 export default {
@@ -26,6 +32,14 @@ export default {
       quantity: 0
     };
   },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
+    }
+  },
   methods: {
     buyStock() {
       const order = {
@@ -33,7 +47,7 @@ export default {
         stockPrice: this.stock.price,
         quantity: this.quantity
       };
-      console.log(order);
+      this.$store.dispatch('buyStock', order);
       this.quantity = 0;
     }
   }
